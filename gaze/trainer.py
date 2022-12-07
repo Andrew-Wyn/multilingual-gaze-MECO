bdef367f2f96daeb8c6a43c0b85c672d8e5502fd
Luca Moroni <terraformer.144@gmail.com>
2022-12-07 23:54:37 +0100

    removed early stopping, added finala retraining

--- gaze/trainer.py
+++ gaze/trainer.py
@@ -1,9 +1,11 @@
+import os
 import torch
 import torch.nn as nn
 import numpy as np
 from tqdm import tqdm
+from collections import defaultdict
 from abc import ABC, abstractmethod
-from gaze.early_stopping import GazeEarlyStopping
+from gaze.tester import GazeTester
 from gaze.utils import LOGGER, mask_mse_loss
 from sklearn.utils import shuffle
 from gaze.utils import LOGGER, create_finetuning_optimizer, create_scheduler, randomize_model, Config
@@ -13,21 +15,21 @@ from sklearn.preprocessing import MinMaxScaler
 
 
 class Trainer(ABC):
-    def __init__(self, cf, model, train_dl, eval_dir, early_stop, task, device, writer):
+    def __init__(self, cf, model, train_dl, eval_dir, tester, task, device, writer):
         self.model = model
         self.train_dl = train_dl
         self.eval_dir = eval_dir
-        self.early_stop = early_stop
         self.n_epochs = cf.n_epochs
         self.task = task
         self.device = device
         self.writer = writer
+        self.tester = tester
 
     @abstractmethod
     def train_one_step(self, batch):
         pass
 
-    def train(self):
+    def train(self, save_model=False):
         n_batches_one_epoch = len(self.train_dl)
         n_params = sum(p.numel() for p in self.model.parameters())
         LOGGER.info(f"Num epochs: {self.n_epochs}")
@@ -53,20 +55,25 @@ class Trainer(ABC):
             LOGGER.info(f"Done epoch {epoch} / {self.n_epochs}")
             LOGGER.info(f"Avg loss epoch {epoch}: {epoch_loss_avg:.4f}")
 
-            # self.early_stop()
+            if self.tester:
+                self.tester.evaluate()
 
-            for key, metric in self.early_stop.tester.metrics.items():
-               self.writer.add_scalar(f"val/{key}", metric, it // n_batches_one_epoch)
-
-            # if self.early_stop.stop:
-            #     break
+                for key, metric in self.tester.metrics.items():
+                    self.writer.add_scalar(f"val/{key}", metric, it // n_batches_one_epoch)
+        
+        # save the model after last epoch
+        if save_model:
+            self.model.save_pretrained(os.path.join(self.dir, "model-"+self.cf.model_pretrained+"-"+str(self.cf.full_finetuning)+"-"+str(self.save_counter)))
 
 
 class GazeTrainer(Trainer):
-    def __init__(self, cf, model, train_dl, val_dl, optim, scheduler, eval_dir,
-                 task, device, monitor, monitor_mode, writer):
-        early_stop = GazeEarlyStopping(cf, model, val_dl, eval_dir, device, task, monitor, monitor_mode, writer)
-        super().__init__(cf, model, train_dl, eval_dir, early_stop, task, device, writer)
+    def __init__(self, cf, model, train_dl, optim, scheduler, eval_dir,
+                 task, device, writer, val_dl=None):
+        if val_dl:
+            tester = GazeTester(model, val_dl, device, task)
+        else:
+            tester = None
+        super().__init__(cf, model, train_dl, eval_dir, tester, task, device, writer)
 
         self.optim = optim
         self.scheduler = scheduler
@@ -104,30 +111,32 @@ def cross_validation(cf, d, eval_dir, writer, DEVICE, k_folds=10):
     l = len(d.text_inputs)
     l_ts = l//k_folds
 
-    loss_tr_mean = 0
-    loss_ts_mean = 0
+    loss_ts_mean = defaultdict(int)
 
     # shuffle dataset with a seed, to take a reproducible sample
-    # dataset = shuffle(dataset, random_state=42)
+    shuffled_ids = shuffle(range(d.text_inputs.shape[0]), random_state=42)
+    text_inputs = d.text_inputs[shuffled_ids]
+    targets = d.targets[shuffled_ids]
+    masks = d.masks[shuffled_ids]
 
     for k in range(k_folds):
         # cicle over folds, for every fold create train_d, valid_d
         if k != k_folds-1: # exclude the k-th part from the validation
 
-            train_inputs = np.append(d.text_inputs[:(k)*l_ts], d.text_inputs[(k+1)*l_ts:], axis=0)
-            train_targets = np.append(d.targets[:(k)*l_ts], d.targets[(k+1)*l_ts:], axis=0)
-            train_masks = np.append(d.masks[:(k)*l_ts], d.masks[(k+1)*l_ts:], axis=0)
-            test_inputs = d.text_inputs[k*l_ts:(k+1)*l_ts]
-            test_targets = d.targets[k*l_ts:(k+1)*l_ts]
-            test_masks = d.masks[k*l_ts:(k+1)*l_ts]
+            train_inputs = np.append(text_inputs[:(k)*l_ts], text_inputs[(k+1)*l_ts:], axis=0)
+            train_targets = np.append(targets[:(k)*l_ts], targets[(k+1)*l_ts:], axis=0)
+            train_masks = np.append(masks[:(k)*l_ts], masks[(k+1)*l_ts:], axis=0)
+            test_inputs = text_inputs[k*l_ts:(k+1)*l_ts]
+            test_targets = targets[k*l_ts:(k+1)*l_ts]
+            test_masks = masks[k*l_ts:(k+1)*l_ts]
 
         else: # last fold clausole
-            train_inputs = d.text_inputs[:k*l_ts]
-            train_targets = d.targets[:k*l_ts]
-            train_masks = d.masks[:k*l_ts]
-            test_inputs = d.text_inputs[k*l_ts:]
-            test_targets = d.targets[k*l_ts:]
-            test_masks = d.masks[k*l_ts:]
+            train_inputs = text_inputs[:k*l_ts]
+            train_targets = targets[:k*l_ts]
+            train_masks = masks[:k*l_ts]
+            test_inputs = text_inputs[k*l_ts:]
+            test_targets = targets[k*l_ts:]
+            test_masks = masks[k*l_ts:]
 
         # min max scaler the targets
         features = train_targets
@@ -163,15 +172,15 @@ def cross_validation(cf, d, eval_dir, writer, DEVICE, k_folds=10):
         scheduler = create_scheduler(cf, optim, train_dl)
 
         # trainer
-        trainer = GazeTrainer(cf, model, train_dl, test_dl, optim, scheduler, eval_dir, "task",
-                                    DEVICE, monitor="loss_all", monitor_mode="min", writer=writer)
+        trainer = GazeTrainer(cf, model, train_dl, optim, scheduler, eval_dir, "task",
+                                    DEVICE, writer=writer, val_dl=test_dl)
         trainer.train()
         LOGGER.info(f"Training completed task")
 
-        # loss_tr_mean += history["loss_tr"]
-        # loss_ts_mean += history["loss_vl"]
+        for key, metric in trainer.tester.metrics.items():
+            loss_ts_mean[key] += metric
 
-    loss_tr_mean /= k_folds
-    loss_ts_mean /= k_folds
+    for key in loss_ts_mean:
+        loss_ts_mean[key] /= k_folds
 
-    return loss_tr_mean, loss_ts_mean
+    return loss_ts_mean
--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from gaze.dataset import GazeDataset
 from gaze.utils import Config
 from gaze.trainer import cross_validation
 import torch
+from sklearn.utils import shuffle
+from gaze.utils import LOGGER, create_finetuning_optimizer, create_scheduler, randomize_model, Config
+from modeling.custom_bert import BertForTokenClassification
+from gaze.dataloader import GazeDataLoader
+from sklearn.preprocessing import MinMaxScaler
+from gaze.trainer import GazeTrainer
 from transformers import (
     # AutoConfig,
     # AutoModelForTokenClassification,
@@ -70,7 +76,55 @@ def main():
     d = GazeDataset(cf, tokenizer, "datasets/it/all_mean_dataset.csv", "try")
     d.read_pipeline()
 
-    cross_validation(cf, d, eval_dir, writer, DEVICE, k_folds=10)
+    # 10-fold cross-validation
+    test_losses = cross_validation(cf, d, eval_dir, writer, DEVICE, k_folds=10)
+
+    print(test_losses)
+
+    # shuffle dataset with a seed, to take a reproducible sample
+    shuffled_ids = shuffle(range(d.text_inputs.shape[0]), random_state=42)
+
+    text_inputs = d.text_inputs[shuffled_ids]
+    targets = d.targets[shuffled_ids]
+    masks = d.masks[shuffled_ids]
+
+    # retrain over all dataset
+    # min max scaler the targets
+    features = targets
+    scaler = MinMaxScaler(feature_range=[0, d.feature_max])
+    flat_features = [j for i in features for j in i]
+    scaler.fit(flat_features)
+
+    targets = [list(scaler.transform(i)) for i in features]
+
+    # create the dataset
+    train_d = list(zip(text_inputs, targets, masks))
+
+    train_dl = GazeDataLoader(cf, train_d, d.target_pad, mode="train")
+
+    # Model
+    LOGGER.info("initiating model: ")
+    model = BertForTokenClassification.from_pretrained(cf.model_pretrained, num_labels=d.d_out,
+                                    output_attentions=False, output_hidden_states=False)
+
+    if cf.random_weights is True:
+        # initiate Bert with random weights
+        print("randomizing weights")
+        model = randomize_model(model)
+        #print(model.classifier.weight.data)
+
+    # optimizer
+    optim = create_finetuning_optimizer(cf, model)
+
+    # scheduler
+    scheduler = create_scheduler(cf, optim, train_dl)
+
+    # trainer
+    trainer = GazeTrainer(cf, model, train_dl, optim, scheduler, eval_dir, "task",
+                                DEVICE, writer=writer)
+    trainer.train()
+    LOGGER.info(f"Training completed task")
+
 
 if __name__ == "__main__":
     main()
